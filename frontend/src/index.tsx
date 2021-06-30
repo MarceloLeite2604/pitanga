61747cbd3be98a11fec46755c31c28512292e111
MarceloLeite2604 <marceloleite2604@gmail.com>
2021-06-30 15:14:09 -0300

    Modify project to use hash route

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import '@fontsource/roboto';
 import { ThemeController } from './theme-controller';
 import { App } from './app';
@@ -7,12 +7,16 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 render(
-  <BrowserRouter>
+  /*
+    Github pages is not too friendly for single page applications. We have to use hash route to allow navigation.
+    Looking forward for this issue to be solved: https://github.com/isaacs/github/issues/408
+  */
+  <HashRouter>
     <ThemeController>
       <App />
     </ThemeController>
     <ToastContainer
       position="bottom-right" />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
